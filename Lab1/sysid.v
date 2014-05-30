62fe36823f9be4a6733b30615f0cbfc3260952d6
Mike <mwerezak@gmail.com>
2014-05-30 16:50:35 -0400

    Added EGM to Cyclone II

--- Lab1/sysid.v
+++ Lab1/sysid.v
@@ -36,7 +36,7 @@ module sysid (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1399508713 : 0;
+  assign readdata = address ? 1401480258 : 0;
 
 endmodule
 
